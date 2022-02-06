af079d18e90b257150ac9e73016f8b0ecd8e53f3
salvoelenasposi <salvoelenasposi@gmail.com>
2022-02-06 22:10:37 +0100

    Aggiunto slideshow

--- js/main.js
+++ js/main.js
@@ -42,3 +42,24 @@ function giftModal(giftId) {
     fjs.parentNode.insertBefore(js,fjs);
   }
 }(document,'script','weatherwidget-io-js');
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
