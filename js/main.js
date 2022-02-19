51ffee2addd2770150bc3b448fb8f6897d43c49c
salvoelenasposi <salvoelenasposi@gmail.com>
2022-02-19 22:34:31 +0100

    Slide animation

--- js/main.js
+++ js/main.js
@@ -49,14 +49,14 @@ showSlides();
 
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("img");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].toggleClass("img");
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
 
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  slides[slideIndex-1].toggleClass("transparent")  
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
